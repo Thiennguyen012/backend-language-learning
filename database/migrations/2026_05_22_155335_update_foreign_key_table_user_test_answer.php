d4b5e26e6ccb5a6db79b83d6cfef6f3ecfc407fe
Thiennguyen0124 <thien.php.igb.vn>
2026-05-22 15:55:19 +0700

    fix fk

---
+++ database/migrations/2026_05_22_155335_update_foreign_key_table_user_test_answer.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_test_answer', function (Blueprint $table) {
+            $table->foreign('user_test_attempt_id')
+                ->references('id')
+                ->on('user_test_attempts')
+                ->onDelete('cascade');
+
+            $table->foreign('question_id')
+                ->references('id')
+                ->on('questions')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_test_answer', function (Blueprint $table) {
+            $table->dropForeign(['user_test_attempt_id']);
+            $table->dropForeign(['question_id']);
+        });
+    }
+};
